151f571352c52a8ddfde94431ecbd8c5c88b7f5e
Paul Sonnentag <Paul.Sonnentag@googlemail.com>
2014-06-09 10:12:44 +0200

    Allows user to add actions

--- client/src/robot_arm/core.cljs
+++ client/src/robot_arm/core.cljs
@@ -1,4 +1,5 @@
 (ns robot-arm.core
+  (:require-macros [cljs.core.async.macros :refer [go]])
   (:require [om.core :as om :include-macros true]
             [om.dom :as dom :include-macros true]
             [cljs.core.async :refer [put! chan <!]]))
@@ -15,22 +16,55 @@
   (-> {:actions []}
       (assoc :type type)))
 
-(def app-state (atom {:timelines (map timeline [:transform :rotate :light :grip])}))
-(defn handle-event [type app val])
+(def app-state (atom {:timelines (->> [:transform :rotate :grip :light]
+                                      (map timeline)
+                                      (vec))}))
 
-(defn timelines-view [app owner]
+(defn handle-event [type app data])
+
+(defn robot-app [app owner]
   (reify
-    om/IRender
-    (render [_]
-            (dom/div #js {:className "timelines-group"}
-                     (apply dom/div #js {:className "timelines"}
-                            (om/build-all timeline-view (:timelines app)))))))
+    om/IInitState
+    (init-state [_]
+     {:comm (chan)})
+    om/IWillMount
+    (will-mount [_]
+                (let [comm (om/get-state owner :comm)]
+                  (go (while true
+                        (let [[type data] (<! comm)]
+                          (handle-event type app data))))))
+    om/IRenderState
+    (render-state [_ {:keys [comm]}]
+                  (timelines-view (:timelines app)))))
+
+(defn timelines-view [timelines]
+  (dom/div #js {:className "timelines-group"}
+           (apply dom/div #js {:className "timelines"}
+                  (om/build-all timeline-view timelines))))
+
+
+(defn insert-action-at [t action actions]
+  (let [insertion-index (->> actions
+                             (map #(+ (:duration %) (:delay %)))
+                             (reductions +)
+                             (take-while #(> t %))
+                             (count))
+        [head tail] (split-at insertion-index actions)]
+    (vec (concat head [action] tail))))
+
+(defn add-action [e timeline]
+  (let [t (-> e .-clientX size->t)]
+    (om/transact! timeline :actions #(insert-action-at t {:duration 1 :delay 2} %))))
 
 (defn timeline-view [timeline owner]
   (reify
+    om/IInitState
+    (init-state [_]
+                {:timeline timeline})
     om/IRender
     (render [_]
-            (dom/div #js {:className (str "timeline " (-> timeline :type name)) }
+            (dom/div #js {:className (str "timeline " (-> timeline :type name))
+                          :onClick #(add-action % timeline)}
                      (apply dom/ul #js {:className "actions"}
                             (om/build-all action-view (:actions timeline)))))))
 
@@ -40,16 +74,23 @@
     om/IRender
     (render [_]
             (dom/li #js {:className "action"
-                         :style #js {:marginLeft (-> action :delay size-unit)
-                                     :width (-> action :duration size-unit)}}
+                         :style #js {:marginLeft (-> action :delay t->px)
+                                     :width (-> action :duration t->px)}}
                     (dom/div #js {:className "inner-action"})))))
 
 
 ;; Utils
-(defn size-unit [x]
-  (str (* 65 x) "px"))
+(def SCALE 65)
 
+(defn size->t [x]
+   (/ SCALE x))
+
+(defn t->size [x]
+  (* SCALE x))
+
+(defn t->px [x]
+  (str (* SCALE x) "px"))
 
 ;; Bootstrap
-(om/root timelines-view app-state
+(om/root robot-app app-state
          {:target (. js/document (getElementById "app"))})
