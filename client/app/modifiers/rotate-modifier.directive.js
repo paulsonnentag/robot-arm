115e17116001473eb949690600ba8043521d155a
Paul Sonnentag <Paul.Sonnentag@googlemail.com>
2014-06-12 22:08:01 +0200

    Implements rotate modifier

--- client/app/modifiers/rotate-modifier.directive.js
+++ client/app/modifiers/rotate-modifier.directive.js
@@ -3,4 +3,28 @@
 
   angular.module('robotArm.modifiers.rotateModifier', [])
 
+    .constant('maxRotation', 120)
+
+    .directive('rotateModifier', function () {
+      return {
+        restrict: 'EA',
+        templateUrl: 'app/modifiers/rotate-modifier.html',
+        scope: {
+          data: '='
+        },
+        controller: function ($scope, maxRotation) {
+
+          $scope.$watch('data.rotation', function (rotation, prevRotation) {
+            if (Math.abs(rotation) > maxRotation) {
+              $scope.data.rotation = norm(rotation) * maxRotation;
+            }
+          });
+
+          function norm(x) {
+            return x < 0 ? -1 : 1;
+          }
+        }
+      }
+    });
+
 }());
