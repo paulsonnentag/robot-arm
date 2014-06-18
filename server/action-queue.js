71990b64340407a217e9075adc05432fdb6ff605
Paul Sonnentag <Paul.Sonnentag@googlemail.com>
2014-06-18 16:48:05 +0200

    Adds actionQueue

---
+++ server/action-queue.js
@@ -0,0 +1,33 @@
+var Firebase = require('firebase');
+var es = require('event-stream');
+
+function actionQueue(url) {
+
+  var actionsRef = new Firebase(url);
+
+  var queue = es.through(function (instruction) {
+
+    es.readArray(instruction.actions)
+      .pipe(es.through(function (action) {
+
+        queue.emit('data', action);
+
+      }, function () {
+
+        actionsRef.child(instruction.id).remove();
+
+      }));
+  });
+
+  actionsRef.on('child_added', function (action) {
+    queue.write({
+      id: action.name(),
+      actions: action.val()
+    });
+  });
+
+  return queue;
+}
+
+module.exports = actionQueue;
+
--- server/server.js
+++ server/server.js
@@ -1,24 +1,9 @@
-var http = require('http');
-var express = require('express');
-var morgan = require('morgan');
 var robotArm = require('./robot-arm.js');
+var actionQueue = require('./actionQueue');
 
+var queue = actionQueue('https://robotarm.firebaseio.com/actions');
 
-var app = express();
-var server = http.Server(app);
-var io = require('socket.io').listen(server);
-
-app.use(morgan('dev'));
-app.use(express.static(__dirname + '/client'));
-
-server.listen(8080);
 
 console.log('started server http://localhost:8080');
 
-io.sockets.on('connection', function (socket) {
-  socket.on('command', function (command) {
-    console.log(command);
-    robotArm.sendCommand(command);
-  });
-});
 
